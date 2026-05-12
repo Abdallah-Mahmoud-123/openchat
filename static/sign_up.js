8f3183d579d41a5b6acedad7f003f3de798e22ce
اسئله دينيه <mbody3072@gmail.com>
2026-05-12 15:31:58 +0300

    Add files via upload

--- static/sign_up.js
+++ static/sign_up.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
     checkInputs();
 });
 function checkInputs() {
-    if (nameInput.value.length > 0 && passwordInput.value.length <= 10 && passwordInput.value.length >= 8 && !passwordInput.value.includes(' ') && emailInput.value.includes('@') && emailInput.value.includes('.') && emailInput.value.length > 5 && (maleRadio.checked || femaleRadio.checked)) {
+    if (nameInput.value.length > 0 && passwordInput.value.length <= 10 && passwordInput.value.length >= 8 && !passwordInput.value.includes(' ') && emailInput.value.includes('@') && emailInput.value.includes('.') && emailInput.value.length > 5 && (maleRadio.checked || femaleRadio.checked) && nameInput.value.length < 12) {
         button.disabled = false;
         button.style.backgroundColor = 'blue';
         button.style.cursor = 'pointer';
@@ -41,4 +41,17 @@ async function signUp() {
     let data = await response.json();
     document.getElementById('message').innerText = data.msg;
 
+}
+
+
+function checkSignIn() {
+    if (nameInput.value.length > 0 && passwordInput.value.length <= 10 && passwordInput.value.length >= 8 && !passwordInput.value.includes(' ')) {
+        button.disabled = false;
+        button.style.backgroundColor = 'blue';
+        button.style.cursor = 'pointer';
+    } else {
+        button.disabled = true;
+        button.style.backgroundColor = 'gray';
+        button.style.cursor = 'not-allowed';
+    }
 }
